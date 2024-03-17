32b8bbbd3b37bac92a70e428f74dc1c312481b75
Max Ruuen <max@madebyruuen.com>
2024-03-17 11:38:00 +1100

    Initial project setup
    Scaffold backend and frontend apps
    
    Set up backend proj structure
    
    Add mock data files
    
    Remove unused angular files and set up initial app state
    
    Configure backend dev CORS policy for frontend dev server
    
    Moved dotnet .gitignore file into Backend dir

---
+++ Backend/WebAPI/Program.cs
@@ -0,0 +1,38 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+// Configure CORS for dev (or prod in future)
+string devOriginPolicy = "AllowClientDevServerOrigin";
+string devClientOrigin = "http://localhost:4200";
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(devOriginPolicy, policy =>
+    {
+        policy.WithOrigins(devClientOrigin);
+    });
+});
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseCors(devOriginPolicy);
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
