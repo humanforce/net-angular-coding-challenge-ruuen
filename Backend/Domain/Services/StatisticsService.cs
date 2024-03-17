fe01f113d5a8dc03a15e441571f398308c467a17
Max Ruuen <max@madebyruuen.com>
2024-03-17 13:34:02 +1100

    Add sprint capacity calculation endpoint Add SprintCapacity entity Add static StatisticsService class with helper method for calcing capacity Add endpoint in SprintController to get capacity by sprint id

---
+++ Backend/Domain/Services/StatisticsService.cs
@@ -0,0 +1,29 @@
+using Domain.Entities;
+
+namespace Domain.Services;
+
+public static class StatisticsService
+{
+    public static SprintCapacity CalculateSprintCapacity(IEnumerable<Ticket> sprintTicketData)
+    {
+        double totalCommitted = 0.0;
+        double totalCompleted = 0.0;
+
+        foreach (var ticket in sprintTicketData)
+        {
+            totalCommitted += ticket.Fields.StoryPoints;
+            if (ticket.Fields.Status.Name == "Done")
+            {
+                totalCompleted += ticket.Fields.StoryPoints;
+            }
+        }
+
+        double capacityPercent = (totalCompleted / totalCommitted) * 100;
+        return new SprintCapacity
+        {
+            TotalPointsCommitted = totalCommitted,
+            TotalPointsCompleted = totalCompleted,
+            CapacityPercent = capacityPercent
+        };
+    }
+}
--- Backend/WebAPI/Controllers/SprintController.cs
+++ Backend/WebAPI/Controllers/SprintController.cs
@@ -1,3 +1,5 @@
+using Domain.Entities;
+using Domain.Services;
 using Infrastructure.Repositories;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -10,11 +12,13 @@ namespace WebAPI.Controllers
     {
         private readonly ILogger<SprintController> _logger;
         private readonly ISprintRepository _sprintData;
+        private readonly ITicketRepository _ticketData;
 
-        public SprintController(ILogger<SprintController> logger, ISprintRepository sprintData)
+        public SprintController(ILogger<SprintController> logger, ISprintRepository sprintData, ITicketRepository ticketData)
         {
             _logger = logger;
             _sprintData = sprintData;
+            _ticketData = ticketData;
         }
 
         [HttpGet("currentSprint")]
@@ -22,6 +26,19 @@ namespace WebAPI.Controllers
         {
             var result = _sprintData.GetCurrentOrLast();
             return Ok(result);
+        }
+
+        [HttpGet("{sprintId}/capacity")]
+        public IActionResult GetSprintCapacity(int sprintId)
+        {
+            var sprintTickets = _ticketData.GetAllBySprint(sprintId);
+            if (sprintTickets.Count > 0)
+            {
+                SprintCapacity capacityCalc = StatisticsService.CalculateSprintCapacity(sprintTickets);
+                return Ok(capacityCalc);
+            }
+
+            return NotFound();
         }
     }
 }
