4b49eba585eb698b2ad61005f498c0a55811a8a8
Max Ruuen <max@madebyruuen.com>
2024-03-17 21:20:14 +1100

    Add holiday service, load holiday data from endpoint

--- Frontend/src/app/holiday-list/holiday-list.component.ts
+++ Frontend/src/app/holiday-list/holiday-list.component.ts
@@ -1,11 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, Input, inject } from '@angular/core';
 import { CardModule } from 'primeng/card';
+import { Sprint } from '../types/sprint';
+import { Holiday } from '../types/holiday';
+import { HolidayService } from '../holiday.service';
+import { DatePipe, NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-holiday-list',
   standalone: true,
-  imports: [CardModule],
+  imports: [DatePipe, NgFor, CardModule],
   templateUrl: './holiday-list.component.html',
   styleUrl: './holiday-list.component.css',
 })
-export class HolidayListComponent {}
+export class HolidayListComponent {
+  holidayService: HolidayService = inject(HolidayService);
+  @Input() selectedSprint!: Sprint | null;
+  holidayList: Holiday[] | null = null;
+
+  ngOnChanges(): void {
+    if (this.selectedSprint === null) {
+      return;
+    }
+
+    this.holidayService
+      .getAllInDateRange(
+        this.selectedSprint.startDate,
+        this.selectedSprint.endDate
+      )
+      .subscribe((holidays) => (this.holidayList = holidays));
+  }
+}
