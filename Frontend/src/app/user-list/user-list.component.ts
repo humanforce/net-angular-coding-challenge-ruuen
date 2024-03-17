7bc23eb3fd56afdf5844e8195906f7f38fee0d35
Max Ruuen <max@madebyruuen.com>
2024-03-17 21:33:20 +1100

    Add user service, load user data from endpoint Likely wont have time to implement the avail checking per location in users

--- Frontend/src/app/user-list/user-list.component.ts
+++ Frontend/src/app/user-list/user-list.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CardModule } from 'primeng/card';
 import { TagModule } from 'primeng/tag';
+import { UserService } from '../user.service';
+import { User } from '../types/user';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-user-list',
   standalone: true,
-  imports: [CardModule, TagModule],
+  imports: [NgFor, CardModule, TagModule],
   templateUrl: './user-list.component.html',
   styleUrl: './user-list.component.css',
 })
-export class UserListComponent {}
+export class UserListComponent {
+  userService: UserService = inject(UserService);
+  userData: User[] = [];
+
+  // won't have time to implement the availability flag so not using onChanges here yet
+  ngOnInit(): void {
+    this.userService.getAll().subscribe((users) => (this.userData = users));
+  }
+}
